8d5cc6eb855bb730477af9131af39f3333bf668e
ricardo gomes <ricardo.gomes@liip.ch>
2025-02-25 21:22:46 +0100

    modification on index to display score 0-0 if match wasnt played

--- flaskr/models/matchs.py
+++ flaskr/models/matchs.py
@@ -1,7 +1,7 @@
 from flaskr.models.db import connection
 
 class Matchs:
-    def __init__(self,id_match,date_match,id_home_team,id_away_team,home_score = None, away_score = None, home_team = None, away_team = None):
+    def __init__(self,id_match, date_match, id_home_team, id_away_team, home_score = None, away_score = None, home_team = None, away_team = None):
         self.id_match = id_match
         self.date_match = date_match
         self.id_home_team = id_home_team

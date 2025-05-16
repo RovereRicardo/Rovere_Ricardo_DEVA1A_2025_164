920fb9676f035853b36b610b731b457604cc5655
ricardo gomes <ricardo.gomes@liip.ch>
2025-05-16 15:53:47 +0200

    MAJ Dump, Serveur crash

--- run_app.py
+++ run_app.py
@@ -6,6 +6,6 @@ from flaskr import create_app
 load_dotenv()
 if __name__ == '__main__':
     app = create_app()
-    # debug = os.getenv('FLASK_DEBUG', '0').lower() in ('1', 'true', 'yes')
-    debug = os.getenv('FLASK_DEBUG', '1').lower() in ('1', 'true', 'yes')
+    debug = os.getenv('FLASK_DEBUG', '0').lower() in ('1', 'true', 'yes')
+    # debug = os.getenv('FLASK_DEBUG', '1').lower() in ('1', 'true', 'yes')
     app.run(debug=debug, host=os.getenv('ADRESSE_SRV_FLASK'), port=os.getenv('FLASK_RUN_PORT'))
